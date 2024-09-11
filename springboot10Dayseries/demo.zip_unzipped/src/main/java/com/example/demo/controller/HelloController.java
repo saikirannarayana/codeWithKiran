c01446b01b7beaac112879be53dcef23f289984b
kiran <saikirannsk56@gmail.com>
2024-09-11 22:45:03 +0530

    Day 5- 10day spring boot series

--- springboot10Dayseries/demo.zip_unzipped/src/main/java/com/example/demo/controller/HelloController.java
+++ springboot10Dayseries/demo.zip_unzipped/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exception.ResourceNotFoundException;
 
 @RestController
 public class HelloController {
@@ -9,5 +12,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello world";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloByName(@PathVariable String name){
+        if(name == null){
+           throw new ResourceNotFoundException("name should not be null");
+        }
+        return "Hello"+name;
     }
 }
