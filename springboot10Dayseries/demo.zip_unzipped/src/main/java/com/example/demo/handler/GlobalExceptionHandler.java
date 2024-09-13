47e7e188a3889978fbdb25ad7bdab67c7f2ea148
kiran <saikirannsk56@gmail.com>
2024-09-13 13:51:26 +0530

    Day-6-SpringBoot learning Series

---
+++ springboot10Dayseries/demo.zip_unzipped/src/main/java/com/example/demo/handler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.demo.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.demo.exception.ResourceNotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e){
+
+        logger.error("Resource not found: {}", e.getMessage());
+        
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
